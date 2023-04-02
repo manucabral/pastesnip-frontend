5f2c3329026821d3151c9f9d204dd6ebcfcd2b46
Manu <mcabral@frba.utn.edu.ar>
2023-04-01 23:06:00 -0300

    feat(main): add Apollo Client and GraphQL configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App'
-import './index.css'
+
+const client = new ApolloClient({
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>
-)
+    </ApolloProvider>
+);
