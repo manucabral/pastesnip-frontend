75519083cf3531eaa6ac1573c80464ad9cbd378d
Manu <mcabral@frba.utn.edu.ar>
2023-04-01 23:05:22 -0300

    docs(navbar): add simple docstring for the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
+/**
+ * Navbar component for the application.
+ * @returns {JSX.Element} The Navbar component.
+ */
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between w-full px-4 py-2 sticky top-0 bg-slate-800">
