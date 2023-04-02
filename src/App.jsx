93d95321cdb95144debcbb3df85a4ec0e8c7755a
Manu <mcabral@frba.utn.edu.ar>
2023-04-01 23:04:57 -0300

    feat(app): fetch Q_HELLO from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Navbar from './components/Navbar'
+import { useQuery } from '@apollo/client'
+import { Q_HELLO } from './graphql/queries'
 
 export default function App() {
+    const { loading, error } = useQuery(Q_HELLO);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>An internal error has occurred. Please try again later.</p>;
     return (
         <div className="flex flex-col w-full h-screen bg-slate-900">
             <Navbar />
