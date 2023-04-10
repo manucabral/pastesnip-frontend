6827f2bb46cbd997d8c5a6f0005bbe9d58753e0a
Manu <mcabral@frba.utn.edu.ar>
2023-04-10 11:49:04 -0300

    refactor: remove index from queries

--- src/graphql/queries/index.jsx
+++
@@ -1,8 +0,0 @@
-import { gql } from '@apollo/client';
-
-/**
- * @description A simple query to test the GraphQL endpoint.
- * @returns {string} The string "Hello, world!"
- * @example { hello }
- */
-export const Q_HELLO = gql`{ hello }`;
