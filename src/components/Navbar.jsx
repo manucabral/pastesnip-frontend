b3e1305d0307f84468790705e518db58cfc655bd
Manu <mcabral@frba.utn.edu.ar>
2023-04-10 11:48:13 -0300

    feat: add redirect links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 /**
  * Navbar component for the application.
  * @returns {JSX.Element} The Navbar component.
@@ -7,6 +9,11 @@ export default function Navbar() {
         <nav className="flex items-center justify-between w-full px-4 py-2 sticky top-0 bg-slate-800">
             <div className="flex items-center">
                 <span className="text-xl font-semibold text-white">Pastesnip</span>
+            </div>
+            <div className="flex items-center">
+                <Link to="/" className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Home</Link>
+                <Link to="about" className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">About</Link>
+                <Link to="contact" className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Contact</Link>
             </div>
             <div className="flex items-center">
                 <button className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Login</button>
