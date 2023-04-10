0b1daafa672930b9d4a868ef7dcaa29a8d9abc5e
Manu <mcabral@frba.utn.edu.ar>
2023-04-10 11:47:53 -0300

    feat: add browser router and notification provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App'
+import './styles/global.css'
 
-const client = new ApolloClient({
-    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
-    cache: new InMemoryCache(),
-});
+import { ApolloProvider } from '@apollo/client'
+import { NotificationProvider } from './context/NotificationContext'
+import { client } from './libs/apollo'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ApolloProvider client={client}>
-        <App />
+        <BrowserRouter>
+            <NotificationProvider>
+                <App />
+            </NotificationProvider>
+        </BrowserRouter>
     </ApolloProvider>
 );
