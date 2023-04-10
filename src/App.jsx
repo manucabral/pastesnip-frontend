3b008582dd71ff298e71da75e9862404a6d28183
Manu <mcabral@frba.utn.edu.ar>
2023-04-10 11:47:39 -0300

    feat: add routes and import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Navbar from './components/Navbar'
 import { useQuery } from '@apollo/client'
 import { Q_HELLO } from './graphql/queries'
+import { Routes, Route } from "react-router-dom";
+
+import Layout from './components/Layout'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import SignIn from './pages/SignIn';
 
 export default function App() {
     const { loading, error } = useQuery(Q_HELLO);
+
     if (loading) return <p>Loading...</p>;
     if (error) return <p>An internal error has occurred. Please try again later.</p>;
     return (
-        <div className="flex flex-col w-full h-screen bg-slate-900">
-            <Navbar />
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<h1 className="text-white">Sign Up</h1>} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
     )
 }
