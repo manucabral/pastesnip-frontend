447684fb3f3bb7e45742146417b178a5959363ae
Manu <mcabral@frba.utn.edu.ar>
2023-04-11 20:21:03 -0300

    feat(pages): add maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import Navbar from './components/Navbar'
 import { useQuery } from '@apollo/client'
 import { Q_HELLO } from './graphql/queries'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Layout from './components/Layout'
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import SignIn from './pages/SignIn';
+import Maintenance from './pages/Maintenance';
 
 export default function App() {
+    const navigate = useNavigate();
     const { loading, error } = useQuery(Q_HELLO);
 
     if (loading) return <p>Loading...</p>;
-    if (error) return <p>An internal error has occurred. Please try again later.</p>;
+    if (error) navigate("/maintenance");
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<h1 className="text-white">Sign Up</h1>} />
+                <Route path="/maintenance" element={<Maintenance />} />
                 <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-white text-3xl">Oops!</h1>
+            <p className="text-white">We are currently under maintenance. Please try again later.</p>
+        </div>
+    )
+}
