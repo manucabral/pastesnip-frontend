cb06fd710fdd838f4b47fc958489c9a27d8e8fdb
Manu <mcabral@frba.utn.edu.ar>
2023-04-16 13:34:26 -0300

    feat(queries): add me

--- src/graphql/queries.jsx
+++ src/graphql/queries.jsx
@@ -10,6 +10,16 @@ export const Q_HELLO = gql`{ hello }`;
 /**
  * @description A query to get the user's data.
  * @returns {object} The user's data.
- * @example { user { id, username, email, createdAt, updatedAt } }
- * @todo Add the user's data to the cache.
+ * @example { me { id, username, email, createdAt, updatedAt } }
  */
+export const Q_ME = gql`
+    query {
+        me {
+            id
+            username
+            email
+            createdAt
+            updatedAt
+        }
+    }
+`;
