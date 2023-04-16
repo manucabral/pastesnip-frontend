be050156e93ecbdee6a954f5b03229b306274c71
Manu <mcabral@frba.utn.edu.ar>
2023-04-16 13:09:06 -0300

    feat(component): add loading on login

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,13 +4,15 @@ import { useNotificationContext } from "../context/NotificationContext"
 import { useMutation } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import { M_LOGIN } from "../graphql/mutations";
+import Loading from "../components/Loading";
 
 export default function SignIn() {
     const navigate = useNavigate();
     const { notification, setNotification } = useNotificationContext();
-    const [loginMutation, { loading, error, data }] = useMutation(M_LOGIN);
+    const [loginMutation, { loading }] = useMutation(M_LOGIN);
     const { handleSubmit } = useAuth({ notification, setNotification, loginMutation, navigate });
 
+    if (loading) return <Loading />;
     return (
         <div className="flex flex-col w-full items-center justify-center min-h-fit gap-10">
             <h1 className="text-white text-3xl mt-10">Sign In to your account</h1>
