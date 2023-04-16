80c78d9f85510ca207eba25170a092f66098c45c
Manu <mcabral@frba.utn.edu.ar>
2023-04-16 13:08:33 -0300

    feat: home redirect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,14 @@ export default function Navbar() {
     return (
         <nav className="flex items-center justify-between w-full px-4 py-2 sticky top-0 bg-slate-800">
             <div className="flex items-center">
-                <span className="text-xl font-semibold text-white">Pastesnip</span>
+                <Link to="/" className="text-white text-2xl font-semibold">Pastesnip</Link>
             </div>
             <div className="flex items-center">
                 <Link to="/" className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Home</Link>
-                <Link to="about" className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">About</Link>
-                <Link to="contact" className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Contact</Link>
             </div>
             <div className="flex items-center">
-                <button className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Login</button>
-                <button className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Sign Up</button>
+                <Link to="signin" className="px-4 py-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Login</Link>
+                <Link to="signup" className="px-4 py-2 ml-2 text-sm font-semibold text-white bg-slate-700 rounded hover:bg-slate-600">Register</Link>
             </div>
         </nav>
     )
