4fa4d5fb822ed27ba92683ff0b92f35a18682bbe
Manu <mcabral@frba.utn.edu.ar>
2023-04-18 01:30:11 -0300

    feat: use basename from .env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '/pastesnip-frontend/',
-    server: {
-        port: 3000,
-    },
-})
+export default ({ mode }) => {
+    Object.assign(process.env, loadEnv(mode, process.cwd()))
+    return defineConfig({
+        plugins: [react()],
+        base: `/${process.env.VITE_BASE_URL}/`,
+        server: {
+            port: 3000,
+        },
+    });
+}
