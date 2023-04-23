6e05ef594c9c9b0dc34340b1a643d45cd90d508b
Manu <mcabral@frba.utn.edu.ar>
2023-04-23 14:02:25 -0300

    refactor: remove process env assign

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-    Object.assign(process.env, loadEnv(mode, process.cwd()))
     return defineConfig({
         plugins: [react()],
-        base: `/${process.env.VITE_BASE_URL}/` || "/pastesnip-frontend/",
+        base: '/pastesnip-frontend/',
         server: {
             port: 3000,
         },
