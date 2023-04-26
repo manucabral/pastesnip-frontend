72bcc17dc7ecc2ce349e9babd38139696beea2e5
leoarayav <leandro.araya08@inacapmail.cl>
2023-04-25 20:44:49 -0400

    feat(home): added: about and features section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,11 @@ export default function Home() {
         document.title = "Pastesnip - Your pastes, your way."
     }, [])
     return (
-        <div className="flex flex-col items-center text-center mt-14">
-            <section className="">
-                <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"> 
-                        Paste<span className="text-blue-500">snip</span> 
+        <div>
+            <section>
+                <div className="py-8 px-4 text-center lg:py-16 lg:px-12">
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                        Paste<span className="text-blue-500">snip</span>
                     </h1>
                     <p className="mb-8 text-lg font-normal text-slate-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-300">
                         The best place to store your pastes, share with the community, comment and interact with other users
@@ -20,9 +20,80 @@ export default function Home() {
                         <Link to='/signin' className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             Sign in &rarr;
                         </Link>
-                        <Link to='/signup' className="shadow-lg shadow-blue-900/60 opacity-80 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 focus:ring-4 dark:text-white border-blue-500/60 hover:bg-blue-900 focus:ring-gray-800 hover:shadow-lg hover:shadow-blue-500/70">
+                        <Link to='/signup' className="shadow-lg shadow-blue-900/60 opacity-80 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 focus:ring-4 dark:text-white border-blue-500/60 hover:bg-blue-900 focus:ring-gray-800 hover:shadow-lg hover:shadow-blue-500/70 hover:transition-all hover:delay-[0.1s]">
                             Register
                         </Link>
+                    </div>
+                </div>
+            </section>
+            <section className="flex flex-col bg-blue-900/20 py-20 text-2xl md:text-4xl">
+                <div className="container mx-auto px-11">
+                    <p className="leading-tight max-w-5xl mx-auto text-2xl tracking-tight">
+                        <h2 className="text-blue-500 mb-6 text-4xl">
+                            Here on pastesnip
+                        </h2>
+                        <strong>You can create</strong> your own pastes
+                        and <strong>share them</strong> with the community, is a great way to
+                        share your code with other people and get feedback from them,
+                        the idea behind this website is to make it more convenient
+                        for users to storage their pastes or important issues inside here.
+                    </p>
+                </div>
+            </section>
+            <section class="bg-zinc-900">
+                <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                    <div class="max-w-screen-md mb-8 lg:mb-16">
+                        <h2 class="mb-4 text-4xl tracking-tight text-blue-500"> Designed for your easily </h2>
+                        <p class="m:text-xl text-white">
+                            We have a lot of features that can you make
+                            your code stuff more easily, practical and allows you to feedback from other users
+                            and interact with other people that have the same interests as you.
+                        </p>
+                    </div>
+                    <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                        <div>
+                            <div class="flex justify-center border border-blue-500/60 text-2xl items-center mb-4 w-10 h-10 rounded-md lg:h-12 lg:w-12">
+                                🪶
+                            </div>
+                            <h3 class="mb-2 text-xl font-bold dark:text-white"> Lightweight </h3>
+                            <p class="text-gray-500 dark:text-gray-400">
+                                We have a lightweight design that allows you to navigate easily and
+                                make your experience more enjoyable, you can navigate through the website
+                                without any problem.
+                            </p>
+                        </div>
+                        <div>
+                            <div class="flex justify-center border border-blue-500/60 text-2xl items-center mb-4 w-10 h-10 rounded-md lg:h-12 lg:w-12">
+                                🔒
+                            </div>
+                            <h3 class="mb-2 text-xl font-bold dark:text-white"> Secure </h3>
+                            <p class="text-gray-500 dark:text-gray-400">
+                                We have a secure system that allows you to store your pastes,
+                                you can private and only you can see it or you can make it public
+                                depending as your needs.
+                            </p>
+                        </div>
+                        <div>
+                            <div class="flex justify-center border border-blue-500/60 text-2xl items-center mb-4 w-10 h-10 rounded-md lg:h-12 lg:w-12">
+                                🧠
+                            </div>
+                            <h3 class="mb-2 text-xl font-bold dark:text-white"> Multisyntax </h3>
+                            <p class="text-gray-500 dark:text-gray-400">
+                                You can create pastes with multiple syntaxes, we have a lot of
+                                syntaxes that you can use to make your code more readable and
+                                understandable for other users, you can create even <strong>text files</strong>
+                            </p>
+                        </div>
+                        <div>
+                            <div class="flex justify-center border border-blue-500/60 text-2xl items-center mb-4 w-10 h-10 rounded-md lg:h-12 lg:w-12">
+                                🌍
+                            </div>
+                            <h3 class="mb-2 text-xl font-bold dark:text-white"> Interactivity </h3>
+                            <p class="text-gray-500 dark:text-gray-400">
+                                Pastesnip allows you to interact with other users, you can comment
+                                on their pastes, give them feedback, rate his pastes and more!
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
