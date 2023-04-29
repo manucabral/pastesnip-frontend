8e2fd518b999d89db4f5880e81141fc5786f47f3
leoarayav <leandro.araya08@inacapmail.cl>
2023-04-29 17:21:03 -0400

    refactor(footer): new gradient colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="left-0 font-semibold bottom-0 w-[100%] bg-blue-600/10 text-white text-center py-8">
             <p>
                 Pastesnip &copy; 2023 - Developed by 
-                <a href="https://github.com/TeamParches" className="ml-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-800">
+                <a href="https://github.com/TeamParches" className="ml-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-500">
                     @TeamParches
                 </a>
             </p>
