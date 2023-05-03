5c9157415b469156926821bf5cdb2aaa84377664
Manu <mcabral@frba.utn.edu.ar>
2023-05-03 16:02:56 -0300

    feat(component): remove user check

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,11 @@
 import { Outlet } from "react-router-dom"
 import { useNotificationContext } from "../context/NotificationContext"
-import { useUserContext } from "../context/UserContext"
-
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import Notification from "./Notification"
-import { useEffect } from "react"
 
 export default function Layout() {
-    const { notification, setNotification } = useNotificationContext();
-    const { user } = useUserContext();
-
-    useEffect(() => {
-        if (localStorage.getItem('access') !== null)
-            setNotification({ show: true, message: `You are logged in as test.`, type: "success" });
-        else
-            setNotification({ show: true, message: "You are not logged in.", type: "warning" });
-    }, [user]);
+    const { notification } = useNotificationContext();
 
     return (
         <div className="min-h-screen bg-slate-900 text-white items-center w-full">
