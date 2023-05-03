75f7b15e467259de9fd8831f9927eec7dcd94d43
Manu <mcabral@frba.utn.edu.ar>
2023-05-03 16:03:40 -0300

    refactor: remove unsued console log

--- src/libs/apollo.jsx
+++ src/libs/apollo.jsx
@@ -1,6 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-console.log(import.meta.env.VITE_GRAPHQL_ENDPOINT);
 const client = new ApolloClient({
     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
     cache: new InMemoryCache(),
