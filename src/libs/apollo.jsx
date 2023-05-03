3a9b2694897a4598c6883065b0742681a08fdb8f
Manu <mcabral@frba.utn.edu.ar>
2023-05-03 16:03:09 -0300

    feat(lib): include access token in apollo client

--- src/libs/apollo.jsx
+++ src/libs/apollo.jsx
@@ -4,6 +4,9 @@ console.log(import.meta.env.VITE_GRAPHQL_ENDPOINT);
 const client = new ApolloClient({
     uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
     cache: new InMemoryCache(),
+    headers: {
+        auth: localStorage.getItem('access') || '',
+    }
 });
 
 export { client };
