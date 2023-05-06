4b664f57da71f2e344ae8ef18eddcb2ec171a5e8
Manu <mcabral@frba.utn.edu.ar>
2023-05-05 22:04:10 -0300

    feat(component): add sign out in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom"
 import { FaGithub } from "react-icons/fa"
+import { useUserContext } from '../context/UserContext';
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Navbar component for the application.
  * @returns {JSX.Element} The Navbar component.
  */
 export default function Navbar() {
+    const navigate = useNavigate();
+    const { user, setUser } = useUserContext();
+    const { signOut } = useAuth({ setUser });
+    const onUserSignOut = () => {
+        signOut();
+        navigate('/home');
+    }
     return (
         <nav className="flex bg-gradient-to-r from-blue-400/60 to-blue-400/10 items-center justify-between flex-wrap top-0 sticky p-2 px-8 bg-opacity-80">
             <div className="flex items-center flex-shrink-0 text-white mr-8">
@@ -29,15 +39,33 @@ export default function Navbar() {
                     </a>
                 </div>
                 <div>
-                    <Link to='/signup' className="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-blue-500 hover:border-transparent hover:text-white hover:bg-blue-500/60 mt-4 lg:mt-0">
-                        Sign Up
-                    </Link>
-                    <Link to='/signin' className="inline-block ml-2 text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-indigo-500 hover:border-transparent hover:text-white hover:bg-blue-500/70 mt-4 lg:mt-0 mr-6">
-                        Sign In
-                    </Link>
-                    <Link to='https://www.github.com/pastesnip'>
-                        <FaGithub className="inline-flex flex-shrink-0 text-xl"/>
-                    </Link>
+                    {user.username ? (
+                        <>
+                            <Link to='/profile' className="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-blue-500 hover:border-transparent hover:text-white hover:bg-blue-500/60 mt-4 lg:mt-0">
+                                Profile
+                            </Link>
+                            <button 
+                                onClick={onUserSignOut}
+                                className="inline-block ml-2 text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-indigo-500 hover:border-transparent hover:text-white hover:bg-blue-500/70 mt-4 lg:mt-0 mr-6">
+                                Sign Out
+                            </button>
+                            <Link to='https://www.github.com/pastesnip'>
+                                <FaGithub className="inline-flex flex-shrink-0 text-xl"/>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to='/signup' className="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-blue-500 hover:border-transparent hover:text-white hover:bg-blue-500/60 mt-4 lg:mt-0">
+                                Sign Up
+                            </Link>
+                            <Link to='/signin' className="inline-block ml-2 text-sm px-4 py-2 leading-none border rounded text-blue-400 font-bold border-indigo-500 hover:border-transparent hover:text-white hover:bg-blue-500/70 mt-4 lg:mt-0 mr-6">
+                                Sign In
+                            </Link>
+                            <Link to='https://www.github.com/pastesnip'>
+                                <FaGithub className="inline-flex flex-shrink-0 text-xl"/>
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
