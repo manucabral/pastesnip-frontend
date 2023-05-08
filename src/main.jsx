374f8067b5dbc4128abef6bba49957530ad5b3f3
Manu <mcabral@frba.utn.edu.ar>
2023-05-08 11:35:47 -0300

    refactor: simple quotes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ApolloProvider client={client}>
-        <BrowserRouter basename='/pastesnip-frontend'>
+        <BrowserRouter basename="/pastesnip-frontend">
             <NotificationProvider>
                 <UserProvider>
                     <App />
@@ -19,4 +19,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             </NotificationProvider>
         </BrowserRouter>
     </ApolloProvider>
-);
+)
